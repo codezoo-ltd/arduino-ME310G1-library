974f55f0af6b84d9b857475c90a3c0802310774f
rooney.jang <rooney.jang@codezoo.co.kr>
2024-12-27 21:27:51 +0900

    M2M filesystem Library. Now Debugging
    Signed-off-by: rooney.jang <rooney.jang@codezoo.co.kr>

--- examples/M2M_example/M2M_example.ino
+++ examples/M2M_example/M2M_example.ino
@@ -30,9 +30,7 @@
 #include <ME310.h>
 #include <string.h>
 
-#ifndef ARDUINO_TELIT_SAMD_CHARLIE
-#define ON_OFF 6 /*Select the GPIO to control ON_OFF*/
-#endif
+#define ON_OFF 2 /*Select the GPIO to control ON_OFF*/
 
 using namespace me310;
 /*
@@ -42,7 +40,7 @@ using namespace me310;
  * Uart Serial1(&sercom4, PIN_MODULE_RX, PIN_MODULE_TX, PAD_MODULE_RX, PAD_MODULE_TX, PIN_MODULE_RTS, PIN_MODULE_CTS);
  * ME310 myME310 (Serial1); 
  */
-ME310 myME310;
+ME310 myME310 (Serial1); 
 ME310::return_t rc;     //Enum of return value  methods
 
 char dir[] = "/mod/dir1";
@@ -54,11 +52,12 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   
   Serial.begin(115200);
-  myME310.begin(115200);
+  Serial1.begin(115200);
+  myME310.debugMode(false);
 
   delay(1000);
   Serial.println("Telit Test AT M2M command");
-  myME310.powerOn();
+  myME310.powerOn(ON_OFF);
   Serial.println("ME310 is ON");
 
   Serial.println("AT Command");
