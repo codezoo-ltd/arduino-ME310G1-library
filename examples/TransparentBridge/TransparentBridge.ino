e409fdd176c804c286428fedfdf6b857dcedd726
rooney.jang <rooney.jang@codezoo.co.kr>
2024-12-27 13:54:19 +0900

    Porting the example to work with the CodeZoo ME310G1 Board
    Signed-off-by: rooney.jang <rooney.jang@codezoo.co.kr>

--- examples/TransparentBridge/TransparentBridge.ino
+++ examples/TransparentBridge/TransparentBridge.ino
@@ -28,9 +28,7 @@
 
 #include <ME310.h>
 
-#ifndef ARDUINO_TELIT_SAMD_CHARLIE
-#define ON_OFF 6 /*Select the GPIO to control ON_OFF*/
-#endif
+#define ON_OFF 2 /*Select the GPIO to control ON_OFF*/
 
 using namespace me310;
 /*
@@ -40,7 +38,7 @@ using namespace me310;
  * Uart Serial1(&sercom4, PIN_MODULE_RX, PIN_MODULE_TX, PAD_MODULE_RX, PAD_MODULE_TX, PIN_MODULE_RTS, PIN_MODULE_CTS);
  * ME310 myME310 (Serial1); 
  */
-ME310 myME310;
+ME310 myME310 (Serial1); 
 ME310::return_t rc;     //Enum of return value  methods
 
 
@@ -49,21 +47,22 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 
   Serial.begin(115200);
-  myME310.begin(115200);
+  Serial1.begin(115200);
+  myME310.debugMode(false);
   delay(1000);
 
   Serial.println("TURN ON ME310");
-  myME310.powerOn();
+  myME310.powerOn(ON_OFF);
   Serial.println("ME310 TURNED ON");
   Serial.println("Bridge Communication Enabled");
 
 }
 
 void loop() {
-  while (SerialModule.available()) {
-    Serial.write(SerialModule.read());
+  while (Serial1.available()) {
+    Serial.write(Serial1.read());
   }
   while (Serial.available()) {
-    SerialModule.write(Serial.read());
+    Serial1.write(Serial.read());
   }
 }
