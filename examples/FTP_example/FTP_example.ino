403dfb104a8d322f4e529ab42c75ee6b791623c7
Rooney Jang <pimco01@naver.com>
2025-01-02 01:04:24 +0900

    Porting FTP example
    Signed-off-by: Rooney Jang <pimco01@naver.com>

--- examples/FTP_example/FTP_example.ino
+++ examples/FTP_example/FTP_example.ino
@@ -31,15 +31,21 @@
 #include <ME310.h>
 #include <string.h>
 
-#define APN "APN"
+#define APN "simplio.apn"
 
+/* <server:port> 
+ * address and port of FTP server
+ * (factory default port 21), in the format:
+ * "ipv4" / "ipv4:port"
+ * "ipv6" / "[ipv6]" / "[ipv6]:port"
+ * "dynamic_name" /
+ * "dynamic_name:port" 
+ */
 #define FTP_ADDR_PORT "PORT"
 #define FTP_USER "CLIENTUSER"
 #define FTP_PASS "PASSWORD"
 
-#ifndef ARDUINO_TELIT_SAMD_CHARLIE
-#define ON_OFF 6 /*Select the GPIO to control ON_OFF*/
-#endif
+#define ON_OFF 2 /*Select the GPIO to control ON_OFF*/
 
 using namespace me310;
 /*
@@ -49,7 +55,7 @@ using namespace me310;
  * Uart Serial1(&sercom4, PIN_MODULE_RX, PIN_MODULE_TX, PAD_MODULE_RX, PAD_MODULE_TX, PIN_MODULE_RTS, PIN_MODULE_CTS);
  * ME310 myME310 (Serial1);
  */
-ME310 myME310;
+ME310 myME310 (Serial1);
 ME310::return_t rc;     //Enum of return value  methods
 
 int cID = 1;            //PDP Context Identifier
@@ -62,11 +68,12 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 
   Serial.begin(115200);
-  myME310.begin(115200);
+  Serial1.begin(115200);
+  myME310.debugMode(false);
 
   delay(1000);
   Serial.println("Telit Test AT FTP command");
-  myME310.powerOn();
+  myME310.powerOn(ON_OFF);
   Serial.println("ME310 ON");
 
   Serial.println("AT Command");
