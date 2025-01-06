6aa807b755f58b7e9d939be5643eb31d221287fd
rooney.jang <rooney.jang@codezoo.co.kr>
2025-01-06 18:48:23 +0900

    Ported the Sleep Mode example to work on CodeZoo ME310G Modem
    Signed-off-by: rooney.jang <rooney.jang@codezoo.co.kr>

--- examples/Sleep_mode_example/Sleep_mode_example.ino
+++ examples/Sleep_mode_example/Sleep_mode_example.ino
@@ -3,26 +3,26 @@
 
 /**
   @file
-    ME310.cpp
-    string.h
-    stdio.h
+  ME310.cpp
+  string.h
+  stdio.h
 
   @brief
-    Sample test of the use of AT commands via ME310 library
+  Sample test of the use of AT commands via ME310 library
 
   @details
-    In this example sketch, it is shown how to get the board to sleep mode using the AT + CFUN = 5 command via the ME310 library.\n
+  In this example sketch, it is shown how to get the board to sleep mode using the AT + CFUN = 5 command via the ME310 library.\n
 
   @version
-    1.0.0
+  1.0.0
 
   @note
 
   @author
-    Cristina Desogus
+  Cristina Desogus
 
   @date
-    04/20/2022
+  04/20/2022
  */
 
 
@@ -52,82 +52,85 @@ int val;
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial1.begin(115200);
-  myME310.debugMode(false);
+	Serial.begin(115200);
+	Serial1.begin(115200);
+	myME310.debugMode(false);
 
-  delay(4000);
-  myME310.powerOn(ON_OFF);
-  Serial.println("ME310 is ON");
+	myME310.powerOn(ON_OFF);  /*Deivce Initialization AT --> OK */
 
-  Serial.println("Application start");
-  delay(1000);
-  Serial.print("Set DTR PIN: ");
-  rc = myME310.gpio_control(5,0,9,1);   //this command sets GPIO5 as DTR and save the configuration
-  Serial.println(myME310.return_string(rc));
+	Serial.println("ME310 is ON");
+	Serial.println("Application start");
+	delay(1000);
 
-  /*After the AT#GPIO=5,0,9,1 command, the module must be rebooted.*/
+	Serial.print("Set DTR PIN: ");
+	//this command sets GPIO5 as DTR and save the configuration
+	//default GPIO Setup(GPIO5, INPUT, internal PullDown, Save) --> rc = myME310.gpio_control(5,4,0,1);
+	rc = myME310.gpio_control(5,0,9,1);   
+	Serial.println(myME310.return_string(rc));
+	/*After the AT#GPIO=5,0,9,1 command*/
+	delay(1000);
 
-  myME310.module_reboot();    //issue command AT#REBOOT
-  delay(5000);
-
-  pinMode(DTR_GPIO,OUTPUT);     //this command sets the DTR PIN in OUTPUT mode
-  digitalWrite(DTR_GPIO, LOW);  //this command sets the DTR PIN to LOW level
+	pinMode(DTR_GPIO,OUTPUT);     //this command sets the DTR PIN in OUTPUT mode
+	digitalWrite(DTR_GPIO, LOW);  //this command sets the DTR PIN to LOW level  
+	delay(5000);
 }
+
+
 
 void loop() {
 
-  Serial.print("Value of DTR is: ");
-  val = digitalRead(DTR_GPIO);    //this command reads the DTR PIN value, it is only for control
-  Serial.println(val);
+	Serial.print("Value of DTR is: ");
+	val = digitalRead(DTR_GPIO);    //this command reads the DTR PIN value, it is only for control
+	Serial.println(val);
 
-  Serial.print("CFUN=5 result: ");
-  rc = myME310.set_phone_functionality(5);  //issues AT#CFUN=5 command and wait for answer or timeout
-  Serial.println(myME310.return_string(rc));
+	Serial.print("CFUN=5 result: ");
+	rc = myME310.set_phone_functionality(5);  //issues AT#CFUN=5 command and wait for answer or timeout
+	Serial.println(myME310.return_string(rc));
 
-  myME310.attention();  //issue AT command, for control
-  Serial.print("Control AT command result: ");
-  if(myME310.buffer_cstr(1) != NULL)
-  {
-    Serial.println(myME310.buffer_cstr(1));
-  }
+	myME310.attention();  //issue AT command, for control
+	Serial.print("Control AT command result: ");
+	if(myME310.buffer_cstr(1) != NULL)
+	{
+		Serial.println(myME310.buffer_cstr(1));
+	}
 
-  delay(3000);
-  Serial.println("Move DTR to OFF");
-  digitalWrite(DTR_GPIO, HIGH);   //this command sets the DTR PIN to HIGH level
+	Serial.println("Move DTR to OFF");
+	digitalWrite(DTR_GPIO, HIGH);   //this command sets the DTR PIN to HIGH level
+	delay(5000);  
 
-  Serial.print("Value of DTR: ");
-  val = digitalRead(DTR_GPIO);
-  Serial.println(val);
+	Serial.print("Value of DTR: ");
+	val = digitalRead(DTR_GPIO);
+	Serial.println(val);
 
-  myME310.attention();
-  if(myME310.buffer_cstr(1) == NULL)
-  {
-    Serial.println("Module is in sleep mode");
-  }
-  else
-  {
-    Serial.println("ERROR module is not in sleep mode");
-  }
+	myME310.attention();
+	if(myME310.buffer_cstr(1) == NULL)
+	{
+		Serial.println("Module is in sleep mode");
+	}
+	else
+	{
+		Serial.println("ERROR module is not in sleep mode");
+	}
+	delay(1000);
 
-  delay(1000);
-  Serial.println("Move DTR to ON");
-  digitalWrite(DTR_GPIO, LOW);  //this command sets the DTR PIN to LOW level, it moves the DTR PIN to wake-up the module
+	Serial.println("Move DTR to ON");
+	digitalWrite(DTR_GPIO, LOW);  //this command sets the DTR PIN to LOW level, it moves the DTR PIN to wake-up the module
+	delay(5000);
+	Serial.print("Value of DTR: ");
+	val = digitalRead(DTR_GPIO);
+	Serial.println(val);
+	delay(3000);
 
-  Serial.print("Value of DTR: ");
-  val = digitalRead(DTR_GPIO);
-  Serial.println(val);
-  delay(3000);
-
-  myME310.attention();
-  if(myME310.buffer_cstr(1) != NULL)
-  {
-    Serial.println("Module is awakened");
-  }
-  else
-  {
-    Serial.println("ERROR Module is not awakened");
-  }
-  exit(0);
+	myME310.attention();
+	if(myME310.buffer_cstr(1) != NULL)
+	{
+		Serial.println("Module is awakened");
+	}
+	else
+	{
+		Serial.println("ERROR Module is not awakened");
+	}
+	exit(0);
 
 }
+
