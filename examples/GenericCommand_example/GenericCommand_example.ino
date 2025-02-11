3ff2952e6a2f748a55060083fbade65b867266bd
Rooney Jang <pimco01@naver.com>
2025-02-11 16:58:59 +0900

    Set code alignment
    Signed-off-by: Rooney Jang <pimco01@naver.com>

--- examples/GenericCommand_example/GenericCommand_example.ino
+++ examples/GenericCommand_example/GenericCommand_example.ino
@@ -11,8 +11,9 @@
     Sample test of the use of Generic Command method via ME310 library
 
   @details
-    In this example sketch, the use of method offered by the ME310 library for using AT commands is shown.\n
-    Through this method, it is possible to send a generic AT command or a list of AT commands.\n
+    In this example sketch, the use of method offered by the ME310 library for
+  using AT commands is shown.\n Through this method, it is possible to send a
+  generic AT command or a list of AT commands.\n
 
 
   @version
@@ -26,30 +27,27 @@
   @date
     08/11/2021
 
-  @ported by rooney.jang (rooney.jang@codezoo.co.kr)	
+  @ported by rooney.jang (rooney.jang@codezoo.co.kr)
  */
 #include <Arduino.h>
 #include <ME310.h>
 
 #define ON_OFF 2 /*Select the GPIO to control ON_OFF*/
+#define MDMSerial Serial1
 
 using namespace me310;
 
-ME310 myME310 (Serial1);
+ME310 myME310;
 ME310::return_t myRc;
 void setup() {
   int i = 0;
   const char OKanswer[] = "OK";
 
-  pinMode(ON_OFF, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
-
   Serial.begin(115200);
-  Serial1.begin(115200);
+  MDMSerial.begin(115200);
+  delay(100);
   myME310.debugMode(false);
-  delay(1000);
   Serial.println("Generic AT Command test");
-  delay(1000);
   myME310.powerOn(ON_OFF);
   Serial.println("ME310 is ON");
 
@@ -57,26 +55,30 @@ void setup() {
     ATCommand is the command string which contains the AT commands to send.
     The single command must be separated from the ;.
     The "AT" string must not be repeated.
-	For example: "AT#GPIO=1,1,1;#GPIO=2,1,1;#GPIO=3,1,1"
+        For example: "AT#GPIO=1,1,1;#GPIO=2,1,1;#GPIO=3,1,1"
   */
   const char ATCommand[] = "AT#SWPKGV;#CGMM";
-  Serial.println((String)"Send: " + ATCommand);
-  myRc = myME310.send_command(ATCommand, OKanswer);     //issue a list of command indicated in the string and wait for anwser or timeout
-  Serial.println((String) "Result value: " + myME310.return_string(myRc));  //print the anwser value
-  while(myME310.buffer_cstr(i) != NULL)
-  {
-    Serial.println(myME310.buffer_cstr(i));  //print line of modem answer
+  Serial.println((String) "Send: " + ATCommand);
+  myRc = myME310.send_command(
+      ATCommand, OKanswer); // issue a list of command indicated in the string
+                            // and wait for anwser or timeout
+  Serial.println((String) "Result value: " +
+                 myME310.return_string(myRc)); // print the anwser value
+  while (myME310.buffer_cstr(i) != NULL) {
+    Serial.println(myME310.buffer_cstr(i)); // print line of modem answer
     i++;
   }
 
   const char readCommand[] = "at+cops?";
-  Serial.println((String)"Send: " + readCommand);
-  myRc = myME310.send_command(readCommand, OKanswer);   //issue a command indicated in the string and wait for anwser or timeout
+  Serial.println((String) "Send: " + readCommand);
+  myRc = myME310.send_command(
+      readCommand, OKanswer); // issue a command indicated in the string and
+                              // wait for anwser or timeout
   Serial.println((String) "Result value: " + myME310.return_string(myRc));
   Serial.println(myME310.buffer_cstr(1));
 
   const char testCommand[] = "at+cmee=?";
-  Serial.println((String)"Send: " + testCommand);
+  Serial.println((String) "Send: " + testCommand);
   myRc = myME310.send_command(testCommand, OKanswer);
   Serial.println((String) "Result value: " + myME310.return_string(myRc));
   Serial.println(myME310.buffer_cstr(1));
